29ef6738dc9d7bb8016662e5b71aa96fd8fbdde7
Guest User <Guest@epicodus-3.local>
2018-01-23 15:33:54 -0800

    Add Register viewModels and Views for authentication

---
+++ Texter/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Texter.ViewModels
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class RegisterViewModel
+    {
+		[Required]
+		[EmailAddress]
+		[Display(Name = "Email")]
+		public string Email { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		[Display(Name = "Password")]
+		public string Password { get; set; }
+
+		[DataType(DataType.Password)]
+		[Display(Name = "Confirm password")]
+		[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+		public string ConfirmPassword { get; set; }
+    }
+}
