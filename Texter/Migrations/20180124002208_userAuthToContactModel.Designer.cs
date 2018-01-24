58746af9b55c7ebf619fefe7d994b1c9afd2f23d
Guest User <Guest@epicodus-3.local>
2018-01-23 16:24:54 -0800

    drop db and add auth user to Contact model and recreate

--- Texter/Migrations/20180124002208_userAuthToContactModel.Designer.cs
+++ Texter/Migrations/20180124002208_userAuthToContactModel.Designer.cs
@@ -8,9 +8,10 @@ using Texter.Models;
 namespace Texter.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180124002208_userAuthToContactModel")]
+    partial class userAuthToContactModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2");
